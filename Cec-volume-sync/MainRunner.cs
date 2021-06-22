2374e2304a17fad15c783288a4b5898a502eaba2
thebestnom <shoval.arad@gmail.com>
2021-06-23 01:12:47 +0300

    make this more resilient to listen to device change by default

---
+++ Cec-volume-sync/MainRunner.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+using AudioSwitcher.AudioApi.Observables;
+
+namespace Cec_volume_sync
+{
+    public class MainRunner: IObserver<DeviceChangedArgs>
+    {
+        private IDevice _controlledAudioDevice;
+        private AudioObserver _audioObserver;
+
+        public async Task<int> Run(string[] args)
+        {
+            var audioController = new CoreAudioController();
+           ;
+            if (args.Length >= 1)
+            {
+                _controlledAudioDevice = (await audioController.GetDevicesAsync())
+                    .FirstOrDefault(audioDevice => audioDevice.Name == args[0]);
+            }
+            else
+            {
+                _controlledAudioDevice = audioController.DefaultPlaybackDevice;
+                audioController.AudioDeviceChanged.Subscribe(this);
+            }
+
+            if (_controlledAudioDevice == null)
+            {
+                Console.WriteLine("Could not open a connection to the VSX");
+                return 1;
+            }
+            await _controlledAudioDevice.SetVolumeAsync(90);
+            
+            var p = new CecSharpClient();
+            if (p.Connect(10000))
+            {
+                _audioObserver = new AudioObserver(p);
+                _controlledAudioDevice.VolumeChanged.Subscribe(_audioObserver);
+                Console.WriteLine("Ready");
+                Thread.Sleep(Timeout.Infinite);
+            }
+            else
+            {
+                Console.WriteLine("Could not open a connection to the CEC adapter");
+                return 1;
+            }
+
+            return 0;
+        }
+
+        public void OnCompleted() { }
+
+        public void OnError(Exception error) { }
+
+        public void OnNext(DeviceChangedArgs value)
+        {
+            _controlledAudioDevice = value.Device;
+            _controlledAudioDevice.VolumeChanged.Subscribe(_audioObserver);
+        }
+    }
+}
--- Cec-volume-sync/Program.cs
+++ Cec-volume-sync/Program.cs
@@ -10,38 +10,8 @@ namespace Cec_volume_sync
     {
         static async Task<int> Main(string[] args)
         {
-            CoreAudioDevice controlledAudioDevice;
-            if (args.Length >= 1)
-            {
-                controlledAudioDevice = new CoreAudioController().GetDevices()
-                    .FirstOrDefault(audioDevice => audioDevice.Name == args[0]);
-            }
-            else
-            {
-                controlledAudioDevice = new CoreAudioController().DefaultPlaybackDevice;
-            }
-
-            if (controlledAudioDevice == null)
-            {
-                Console.WriteLine("Could not open a connection to the VSX");
-                return 1;
-            }
-            await controlledAudioDevice.SetVolumeAsync(90);
-            
-            var p = new CecSharpClient();
-            if (p.Connect(10000))
-            {
-                controlledAudioDevice.VolumeChanged.Subscribe(new AudioObserver(p));
-                Console.WriteLine("Ready");
-                Thread.Sleep(Timeout.Infinite);
-            }
-            else
-            {
-                Console.WriteLine("Could not open a connection to the CEC adapter");
-                return 1;
-            }
-
-            return 0;
+            var main = new MainRunner();
+            return await main.Run(args);
         }
     }
 }
